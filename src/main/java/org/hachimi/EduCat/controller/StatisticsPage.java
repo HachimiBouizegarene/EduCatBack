22e50a1774b8553925f3a10072156d2247379ca1
Mahydine <mlazzouli.pro@gmail.com>
2024-01-06 17:57:45 +0100

    add getPartiesInfos

--- src/main/java/org/hachimi/EduCat/controller/StatisticsPage.java
+++ src/main/java/org/hachimi/EduCat/controller/StatisticsPage.java
@@ -1,9 +1,36 @@
 package org.hachimi.EduCat.controller;
 
+import org.hachimi.EduCat.Exceptions.InformationsException;
+import org.hachimi.EduCat.repository.DataService;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatisticsPage {
 
+    private final DataService dataService;
 
+    public StatisticsPage(DataService dataService){
+        this.dataService = dataService;
+    }
+
+    @PostMapping(path = "/getPartiesInfos")
+    public String getPartiesInfos(@RequestBody String body) throws InformationsException {
+        JSONObject json_body = new JSONObject(body);
+        JSONArray parties;
+        String IdUser;
+
+        try{
+            IdUser = json_body.getString("IdUser");
+            parties = dataService.getTableData("Partie", new JSONObject().put("IdUser", IdUser));
+        }catch (JSONException e){
+            throw new InformationsException();
+        }
+
+        return parties.toString();
+    }
 }
